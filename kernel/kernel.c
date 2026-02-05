8306f929ac237d303cd0bd4fd9731591d643eefc
Nicolas-Rvr25 <nicoriviere25@gmail.com>
2026-02-05 14:18:14 +0100

    FIX: Makefile fclean

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,6 +7,7 @@ void kmain(void)
     for (int i = 0; i < 80 * 25 * 2; i++)
         video_memory[i] = 0;
     print_string("Kernel C loaded successfully!");
+    print_char(' ', 10,2, 0x4f);
     while(1);
 }
 
